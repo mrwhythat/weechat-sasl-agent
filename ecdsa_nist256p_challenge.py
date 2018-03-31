7f068075ac51b9bf5fc7580dc688342c2dfc8089
whythat <whythat@protonmail.com>
2018-03-31 22:43:11 +0300

    add agent implementing ECDSA_NIST256P_CHALLENGE mechanism

---
+++ ecdsa_nist256p_challenge.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python
+
+#
+# WeeChat SASL agent for DELEGATED SASL mechanism
+#
+
+import sys
+import base64
+
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.serialization import load_pem_private_key
+
+
+def nist256p_challenge(key, challenge):
+    """
+    Compute NIST256P signature over the given challenge data
+
+    Assume 'challenge' data is base64-encoded string and 'key'
+    is a base64-encoded private key to be used for signing.
+    """
+    # TODO: add if challenge == '+' logic (see weechat's NIST256P impl.)
+    backend = default_backend()
+    data = base64.b64decode(challenge)
+    private_key = load_pem_private_key(key.encode('ascii'), None, backend)
+    signature = private_key.sign(data, ec.ECDSA(hashes.SHA256()))
+    return base64.b64encode(signature).decode('ascii')
+
+
+def main(args):
+    """
+    SASL DELEGATED protocol logic:
+        - if called without arguments, return registered (supported)
+          name of the mechanism (ecdsa_nist256p_challenge in this case);
+        - if called with two arguments, assume it's path to the key and
+          base64-encoded challenge data and return the signature of the
+          given data with the given key;
+        - otherwise return empty string (shouldn't happen).
+    """
+    if len(args) == 1:
+        print('ecdsa_nist256p_challenge')
+    elif len(args) == 3:
+        keyfile, challenge = args[1], args[2]
+        with open(keyfile, 'r') as f:
+            print(nist256p_challenge(f.read(), challenge))
+    else:
+        print('')
+
+
+if __name__ == '__main__':
+    main(sys.argv)
